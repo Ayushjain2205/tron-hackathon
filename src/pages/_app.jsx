c0391cc36f527dd474ef54c49ee51c0c53d1378f
Ayush Jain <ayushjain2205@gmail.com>
2023-08-14 13:01:58 +0530

    Fix production build break

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import React, { useState } from "react";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import {
   OptimismGoerli,
@@ -14,18 +13,16 @@ import { EcoSystemContext } from "../contexts/EcoSystemContext";
 function App({ Component, pageProps }) {
   const [ecosystem, setEcosystem] = useState("optimism");
   return (
-    <UserProvider>
-      <ThirdwebProvider
-        activeChain={OptimismGoerli}
-        supportedChains={[OptimismGoerli, BaseGoerli, ZoraTestnet, ModeTestnet]}
-        clientId="5dbb8f1abc71686e44ab6ff02c6a9a92"
-        supportedWallets={[metamaskWallet(), localWallet()]}
-      >
-        <EcoSystemContext.Provider value={{ ecosystem, setEcosystem }}>
-          <Component {...pageProps} />
-        </EcoSystemContext.Provider>
-      </ThirdwebProvider>
-    </UserProvider>
+    <ThirdwebProvider
+      activeChain={OptimismGoerli}
+      supportedChains={[OptimismGoerli, BaseGoerli, ZoraTestnet, ModeTestnet]}
+      clientId="5dbb8f1abc71686e44ab6ff02c6a9a92"
+      supportedWallets={[metamaskWallet(), localWallet()]}
+    >
+      <EcoSystemContext.Provider value={{ ecosystem, setEcosystem }}>
+        <Component {...pageProps} />
+      </EcoSystemContext.Provider>
+    </ThirdwebProvider>
   );
 }
 
