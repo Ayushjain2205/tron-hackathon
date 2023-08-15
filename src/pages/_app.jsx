1c1f836697f0a144ad4a15dc92cccfe1d515278c
Ayush Jain <ayushjain2205@gmail.com>
2023-08-15 11:22:19 +0530

    Add email wallet

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,7 @@ import {
   ZoraTestnet,
   ModeTestnet,
 } from "@thirdweb-dev/chains";
-import { metamaskWallet, localWallet } from "@thirdweb-dev/react";
+import { metamaskWallet, localWallet, paperWallet } from "@thirdweb-dev/react";
 import { EcoSystemContext } from "../contexts/EcoSystemContext";
 
 function App({ Component, pageProps }) {
@@ -17,7 +17,13 @@ function App({ Component, pageProps }) {
       activeChain={OptimismGoerli}
       supportedChains={[OptimismGoerli, BaseGoerli, ZoraTestnet, ModeTestnet]}
       clientId="5dbb8f1abc71686e44ab6ff02c6a9a92"
-      supportedWallets={[metamaskWallet(), localWallet()]}
+      supportedWallets={[
+        metamaskWallet(),
+        localWallet(),
+        paperWallet({
+          clientId: "7f3a5a69-a739-40cd-b62d-8ef3667a81b8",
+        }),
+      ]}
     >
       <EcoSystemContext.Provider value={{ ecosystem, setEcosystem }}>
         <Component {...pageProps} />
