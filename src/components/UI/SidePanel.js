76e29c96ae13fa813344057ef4e81d119e7828f0
Ayush Jain <ayushjain2205@gmail.com>
2023-08-16 18:30:49 +0530

    Change linea and etherum config

--- src/components/UI/SidePanel.js
+++ src/components/UI/SidePanel.js
@@ -5,8 +5,8 @@ const SidePanel = ({ setInputValue, setResponseText, setOutputContent }) => {
   const { ecosystem } = useContext(EcoSystemContext);
 
   const colors = {
-    ethereum: { border: "#627EEA", bg: "#627EEA", text: "black" },
-    linea: { border: "#62DFFF", bg: "#62DFFF", text: "white" },
+    ethereum: { border: "#627EEA", bg: "#627EEA", text: "white" },
+    linea: { border: "#62DFFF", bg: "#62DFFF", text: "black" },
     polygon: { border: "#8a46ff", bg: "#8a46ff", text: "white" },
     optimism: { border: "#fff", bg: "#FF0420", text: "white" },
   };
