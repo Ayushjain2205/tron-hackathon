acdce6e3ce9c82f17d56111b941389ca84af7a9c
Ayush Jain <ayushjain2205@gmail.com>
2023-08-17 22:26:26 +0530

    Default bridge params

--- src/components/Custom/CelerNetwork.js
+++ src/components/Custom/CelerNetwork.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CelerNetwork = () => (
   <iframe
-    src="https://cbridge.celer.network/1/1284/ETH"
+    src="https://cbridge.celer.network/1/59144/iUSD"
     width="600px"
     height="650px"
     style={{ border: "none" }}
--- src/pages/api/ai.js
+++ src/pages/api/ai.js
@@ -64,6 +64,6 @@ export default async function handler(req, res) {
   // }
 
   setTimeout(() => {
-    res.status(200).json({ message: "reminder" });
+    res.status(200).json({ message: "bridge" });
   }, 1000);
 }
