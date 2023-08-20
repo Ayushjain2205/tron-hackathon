013d6862fd409e2d089fa8903133d059bc02f29a
Ayush Jain <ayushjain2205@gmail.com>
2023-08-21 01:54:41 +0530

    Update panel items

--- src/data/panel_items.js
+++ src/data/panel_items.js
@@ -1,50 +1,49 @@
 const panel_items = [
   {
     title: "Wallet Health",
-    prompt:
-      "Create a step-by-step guide on how to deploy a sample contract using thirdweb",
+    prompt: "Check my wallet health and point out any issues",
+    img: "/cats/cat5.svg",
+  },
+  {
+    title: "Deploy smart contract",
+    prompt: "Help me create a NFT collection on Linea network",
     img: "/cats/cat3.svg",
+  },
+  {
+    title: "Generate NFTS!!!",
+    prompt: "I want to mint a NFT",
+    img: "/cats/cat2.svg",
   },
   {
     title: "User manual",
     prompt:
       "Create a step-by-step guide on how to deploy a sample contract using thirdweb",
+    img: "/speech-bubble.svg",
+  },
+  {
+    title: "Upcoming Airdrops",
+    prompt: "Help me create a NFT collection on mode network",
+    img: "/cats/cat4.svg",
+  },
+  {
+    title: "Bridge Funds",
+    prompt: "I want to bridge ETH from Ethereum to Linea",
     img: "/cats/cat5.svg",
   },
   {
-    title: "Generate NFTS!!!",
-    prompt: "I want to mint a NFT",
+    title: "Send money",
+    prompt: "I want to transfer 0.1 ETH to Joe",
+    img: "/cats/cat3.svg",
+  },
+  {
+    title: "Scout for NFTs",
+    prompt: "Remind me to scout for NFTs at 12pm",
     img: "/cats/cat2.svg",
   },
   {
-    title: "User Manual",
-    prompt: "",
-    img: "/speech-bubble.svg",
-  },
-  {
-    title: "Deploy smart contract",
-    prompt: "Help me create a NFT collection on mode network",
-    img: "/cats/cat3.svg",
-  },
-  {
-    title: "Deploy smart contract",
-    prompt: "Help me create a NFT collection on mode network",
-    img: "/cats/cat3.svg",
-  },
-  {
-    title: "Generate NFTS!!!",
-    prompt: "I want to mint a NFT",
-    img: "/cats/cat2.svg",
-  },
-  {
-    title: "Deploy smart contract",
-    prompt: "Help me create a NFT collection on mode network",
-    img: "/cats/cat3.svg",
-  },
-  {
-    title: "User Manual",
-    prompt: "",
-    img: "/speech-bubble.svg",
+    title: "Buy NFT",
+    prompt: "I want to buy a NFT",
+    img: "/cats/cat4.svg",
   },
 ];
 
