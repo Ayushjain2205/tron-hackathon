b5c87d2efd4ac83dfa141084a8dc962a356630de
Ayush Jain <ayushjain2205@gmail.com>
2023-08-20 15:46:53 +0530

    Update loaders

--- src/components/Custom/Loader.js
+++ src/components/Custom/Loader.js
@@ -1,27 +1,29 @@
 import React, { useContext } from "react";
 import Lottie from "react-lottie-player";
-import mode from "../../helpers/mode.json";
-import zora from "../../helpers/zora.json";
-import optimisim from "../../helpers/optimisim.json";
-import base from "../../helpers/base.json";
+
+// Import animation data
+import optimismAnimation from "../../helpers/optimisim.json";
+import ethereumAnimation from "../../helpers/ethereum.json";
+import polygonAnimation from "../../helpers/polygon.json";
+import lineaAnimation from "../../helpers/linea.json";
 import { EcoSystemContext } from "../../contexts/EcoSystemContext";
 
 const Loader = () => {
   const { ecosystem } = useContext(EcoSystemContext);
 
+  // Choose the right animation data based on the ecosystem
+  const animationData = {
+    ethereum: ethereumAnimation,
+    polygon: polygonAnimation,
+    optimism: optimismAnimation,
+    linea: lineaAnimation,
+  };
+
   return (
     <div>
       <Lottie
         loop
-        animationData={
-          ecosystem === "ethereum"
-            ? mode
-            : ecosystem === "polygon"
-            ? zora
-            : ecosystem === "optimism"
-            ? optimisim
-            : base
-        }
+        animationData={lineaAnimation} // Default to ethereum animation if the ecosystem is not recognized
         play
         style={{ width: 117, height: 117 }}
       />
