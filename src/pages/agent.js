2eddc517713a6544f0032f6317aa72b03aabe5a7
Ayush Jain <ayushjain2205@gmail.com>
2023-08-20 18:41:33 +0530

    Refactor prompt box, add arrow and onclick fetch

--- src/pages/agent.js
+++ src/pages/agent.js
@@ -43,30 +43,34 @@ const chat = () => {
     return foundItem ? foundItem[text] : null;
   };
 
+  const fetchAndProcessResponse = async () => {
+    setIsLoading(true);
+    // Execute fetch
+    const response = await fetch("/api/ai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: inputValue }),
+    });
+    const data = await response.json();
+    console.log(data);
+    setOutput(data.message);
+    setResponseText(data.message);
+    setIsLoading(false);
+    console.log(responseIntro[responseText]);
+  };
+
   const handleKeyPress = async (e) => {
     if (e.key === "Enter") {
-      setIsLoading(true);
-      // Execute fetch when Enter key is pressed
-      const response = await fetch("/api/ai", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ query: inputValue }),
-      });
-      const data = await response.json();
-      console.log(data);
-      setOutput(data.message);
-      setResponseText(data.message);
-      setIsLoading(false);
-      console.log(responseIntro[responseText]);
+      fetchAndProcessResponse();
     }
   };
   return (
     <Layout theme="chat">
       <div className="flex flex-row gap-[228px] min-h-[852px] pt-[40px] text-[#D1D2D3]">
         <div className="flex flex-col w-[878px]">
-          <input
+          {/* <input
             value={inputValue}
             onChange={handleInputChange}
             onKeyDown={handleKeyPress}
@@ -75,7 +79,41 @@ const chat = () => {
             placeholder="What may I do for you?"
             name=""
             id=""
-          />
+          /> */}
+          <div className="relative w-[878px] h-[54px] mb-[24px]">
+            <input
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyPress}
+              type="text"
+              className="w-full h-full overflow-y-auto resize-none bg-transparent border-b-2 border-[#F1F1F1] focus:outline-none text-[32px] font-medium pr-10"
+              placeholder="What may I do for you?"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              viewBox="0 0 36 36"
+              fill="none"
+              className="absolute top-1/2 transform -translate-y-1/2 right-1 cursor-pointer"
+              onClick={fetchAndProcessResponse}
+            >
+              <path
+                d="M7.5 18H28.5"
+                stroke="#D1D2D3"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M18 7.5L28.5 18L18 28.5"
+                stroke="#D1D2D3"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
           <div className="output">
             {isLoading && (
               <div className="flex flex-row justify-center items-center">
