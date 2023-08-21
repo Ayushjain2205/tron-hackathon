4de9dc0f30f53e159b0f96adf2ced18ac1653af9
Ayush Jain <ayushjain2205@gmail.com>
2023-08-21 12:02:47 +0530

    update networks

--- src/components/Templates/Create.js
+++ src/components/Templates/Create.js
@@ -89,9 +89,10 @@ const Create = () => {
           </p>
           <div>
             <select className="select select-bordered border-[#fff] bg-[#fff] rounded-[12px] w-[545px] h-[60px] text-black font-normal text-[16px] focus:outline-none">
-              <option selected>Optimism</option>
-              <option>Base</option>
-              <option>Zora</option>
+              <option selected>Ethereum</option>
+              <option>Linea</option>
+              <option>Polygon</option>
+              <option>Optimism</option>
             </select>
           </div>
           <div className="flex flex-row justify-between items-center  mt-[105px]">
