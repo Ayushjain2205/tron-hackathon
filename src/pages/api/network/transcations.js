d7c4a63914324658b295b2b1d018333e43c6fa28
Ayush Jain <ayushjain2205@gmail.com>
2023-08-21 21:39:58 +0530

    Add networks api endpoints

---
+++ src/pages/api/network/transcations.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export default async (req, res) => {
+  const address = req.query.address;
+  const network = req.query.network;
+
+  if (!address) {
+    return res
+      .status(400)
+      .json({ error: "Address query parameter is required." });
+  }
+
+  if (!network) {
+    return res
+      .status(400)
+      .json({ error: "Network query parameter is required." });
+  }
+
+  let endpoint;
+
+  switch (network) {
+    case "linea":
+      endpoint = `https://linea-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+      break;
+    case "ethereum":
+      endpoint = `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+      break;
+    case "polygon":
+      endpoint = `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+      break;
+    case "optimism":
+      endpoint = `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+      break;
+    default:
+      return res.status(400).json({ error: "Invalid network specified." });
+  }
+
+  const data = {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getTransactionCount",
+    params: [address, "latest"],
+  };
+
+  try {
+    const response = await axios.post(endpoint, data);
+    return res.status(200).json({ count: parseInt(response.data.result, 16) });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Failed to fetch transaction count." });
+  }
+};
