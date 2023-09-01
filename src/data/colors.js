a91c5a50747c8159aefda15a94925ce3c66181c1
Ayush Jain <ayushjain2205@gmail.com>
2023-09-01 17:28:57 +0530

    Clean logos and modify theme colors

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,6 +1,5 @@
 const colors = {
-  //ethereum: { border: "#627EEA", bg: "#627EEA", text: "white" },
-  bnb: { border: "#FCD435", bg: "#FCD435", text: "white" },
+  bnb: { border: "#FCD435", bg: "#FCD435", text: "black" },
   opbnb: { border: "#CC9D07", bg: "#CC9D07", text: "white" },
   greenfield: { border: "#00BA34", bg: "#00BA34", text: "white" },
 };
