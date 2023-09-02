7f3be0e38650929c053fbb8ec6eb6db97f0fe425
Ayush Jain <ayushjain2205@gmail.com>
2023-09-02 23:33:56 +0530

    Refactor steps component

--- src/components/Steps/Step3.js
+++ src/components/Steps/Step3.js
@@ -1,41 +1,43 @@
 import React from "react";
 
+const CategoryTag = ({ label, isSelected = false }) => {
+  return (
+    <span
+      className={`rounded-[4px] px-[20px] py-[4px] text-[20px] ${
+        isSelected ? "border border-black" : "bg-white"
+      }`}
+    >
+      {label}
+    </span>
+  );
+};
+
 const Step3 = () => {
+  const categories = [
+    "All",
+    "Music",
+    "Art",
+    "Sports",
+    "Media",
+    "Movies",
+    "AI",
+    "Entertainment",
+    "Generative",
+    "Collectible",
+  ];
+
   return (
     <div className="flex flex-col h-[539px] gap-[24px] items-center justify-center">
       <div className="w-[778px] h-[212px] bg-[#f8f8f8] p-[16px] rounded-[8px]">
         <p className="text-[24px] mb-[24px]">Pick categories</p>
         <div className="flex flex-wrap gap-[12px] cursor-pointer">
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            All
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Music
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px] border border-black">
-            Art
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Sports
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Media
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Movies
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            AI
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Entertainment
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Generative
-          </span>
-          <span className="rounded-[4px] bg-white px-[20px] py-[4px] text-[20px]">
-            Collectible
-          </span>
+          {categories.map((category, index) => (
+            <CategoryTag
+              key={index}
+              label={category}
+              isSelected={category === "Art"}
+            />
+          ))}
         </div>
       </div>
     </div>
