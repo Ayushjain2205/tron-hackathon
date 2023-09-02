6c5a29cfb3edcfcb8091f599713c06cc47eba55b
Ayush Jain <ayushjain2205@gmail.com>
2023-09-02 16:52:17 +0530

    Refactor theme helpers

--- src/utils/getColor.js
+++ src/utils/getColor.js
@@ -5,8 +5,6 @@ import colors from "../data/colors";
 const getColor = () => {
   const { ecosystem } = useContext(EcoSystemContext);
 
-  // Check if the ecosystem exists in the colors object
-  // and return the background color or return a default color if not found
   return colors[ecosystem] ? colors[ecosystem].bg : "#ffffff";
 };
 
--- src/utils/getTextColor.js
+++ src/utils/getTextColor.js
@@ -5,8 +5,6 @@ import colors from "../data/colors";
 const getTextColor = () => {
   const { ecosystem } = useContext(EcoSystemContext);
 
-  // Check if the ecosystem exists in the colors object
-  // and return the background color or return a default color if not found
   return colors[ecosystem] ? colors[ecosystem].text : "#ffffff";
 };
 
