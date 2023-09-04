558e22eda6a5fd400561f4aa66058324b570b6ff
Ayush Jain <ayushjain2205@gmail.com>
2023-09-04 12:23:30 +0530

    Add testing route for deploy button

--- src/components/Custom/DeployButton.js
+++ src/components/Custom/DeployButton.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSDK } from "@thirdweb-dev/react";
 
 const DeployButton = () => {
-  return <button>Deploy Button</button>;
+  const sdk = useSDK();
+
+  async function deployContract() {
+    sdk?.deployer.deployNFTDrop({
+      name: "My NFT Drop",
+      primary_sale_recipient: "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0",
+    });
+  }
+
+  return <button onClick={deployContract}>Deploy Button</button>;
 };
 
 export default DeployButton;
---
+++ src/pages/testing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/UI/Layout";
+import DeployButton from "../components/Custom/DeployButton";
+
+const testing = () => {
+  return (
+    <Layout>
+      <div>
+        <DeployButton />
+      </div>
+    </Layout>
+  );
+};
+
+export default testing;
