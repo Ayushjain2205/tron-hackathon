6f152a76ee494458ddc46c47c48317a5365ae398
Ayush Jain <ayushjain2205@gmail.com>
2023-09-04 20:45:06 +0530

    Toast success and errors while signing and deploying

--- src/components/Buttons/DeployButton.js
+++ src/components/Buttons/DeployButton.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSDK } from "@thirdweb-dev/react";
 import { useWallet } from "../../contexts/WalletContext";
+import { toast } from "react-hot-toast";
 
 const DeployButton = () => {
   const [deploying, setDeploying] = useState(false);
@@ -16,11 +17,18 @@ const DeployButton = () => {
   async function deployContract() {
     const isConnected = handleCheckWallet();
     if (isConnected) {
-      setDeploying(true);
-      sdk?.deployer.deployNFTDrop({
-        name: "My NFT Drop",
-        primary_sale_recipient: "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0",
-      });
+      try {
+        setDeploying(true);
+        await sdk?.deployer.deployNFTDrop({
+          name: "My NFT Drop",
+          primary_sale_recipient: "0xCafa93E9985793E2475bD58B9215c21Dbd421fD0",
+        });
+        toast.success("Transcation successful!");
+        setDeploying(false);
+      } catch (e) {
+        toast.error("Transcation Rejected");
+        setDeploying(false);
+      }
     }
   }
 
