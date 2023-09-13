995e3743286c814c46fb70ef9ef91adc4fc74798
Ayush Jain <ayushjain2205@gmail.com>
2023-09-14 00:06:40 +0530

    Add title header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="normal">
       <Head>
+        <title>Ally.ai</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
